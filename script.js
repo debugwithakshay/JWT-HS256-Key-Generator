232d71bf16de9fb051ed6637a294fb236c8d1e3f
Akshay Yadav <debugwithakshay@gmail.com>
2024-05-16 15:10:29 +0530

    replaced md5 with SHA256

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-const md5 = require("md5");
+const crypto = require('crypto');
 
-//Note: don't forget to change key variables
-//generate secret passphrase using https://www.useapassphrase.com/
-//generate salt using https://www.random.org/passwords/?num=3&len=32&format=html&rnd=new
+// Note: Don't forget to change key variables
+// Generate secret passphrase using https://www.useapassphrase.com/
+// Generate salt using https://www.random.org/passwords/?num=3&len=32&format=html&rnd=new
 
-//--------------key variables-------------
-const secret_1 =
-  "purge dagger rants unwanted recreate cheek slimness livestock throwback haiku staple polo";
-const secret_2 =
-  "attention unsocial unwritten pessimism abnormal prepay equator mop crowbar vanquish frigidity padding";
-const secret_3 =
-  "pointed irritable doorstop musky finer valley down filler magnitude cheek pungent viselike";
-const secret_4 =
-  "slacking strep angled unmasking eastcoast overpass cassette stifle tradition undying sixteen obscure";
+// -------------- Key variables -------------
+const secret_1 = "purge dagger rants unwanted recreate cheek slimness livestock throwback haiku staple polo";
+const secret_2 = "attention unsocial unwritten pessimism abnormal prepay equator mop crowbar vanquish frigidity padding";
+const secret_3 = "pointed irritable doorstop musky finer valley down filler magnitude cheek pungent viselike";
+const secret_4 = "slacking strep angled unmasking eastcoast overpass cassette stifle tradition undying sixteen obscure";
 
-const seperator = ".+.";
-const seprator_salt1 = "yTPC73XAjcJDypU7RGvhUap4aUBGYXpW";
-const seprator_salt2 = "tavvaL6Kd3dy87FPFaVzmyDkN4sJWwhH";
-const seprator_salt3 = "V9qjmyys2zkGaKJ9L3SP5kH7KM39R4VA";
-const final_pepper = "your_custom_salt"
-//--------------------------------------------
+const separator = ".+.";
+const separator_salt1 = "yTPC73XAjcJDypU7RGvhUap4aUBGYXpW";
+const separator_salt2 = "tavvaL6Kd3dy87FPFaVzmyDkN4sJWwhH";
+const separator_salt3 = "V9qjmyys2zkGaKJ9L3SP5kH7KM39R4VA";
+const final_pepper = "your_custom_pepper";
+// --------------------------------------------
 
-const md5_secret_1 = md5(secret_1);
-const md5_secret_2 = md5(secret_2);
-const md5_secret_3 = md5(secret_3);
-const md5_secret_4 = md5(secret_4);
-const md5_seprator_with_salt1 = md5(`${seperator}${seprator_salt1}`);
-const md5_seprator_with_salt2 = md5(`${seperator}${seprator_salt2}`);
-const md5_seprator_with_salt3 = md5(`${seperator}${seprator_salt3}`);
-const final_pepper_hash = md5(final_pepper)
+const hash = (data) => crypto.createHash('sha256').update(data).digest('hex');
 
-const original_key = `${md5_secret_1}${md5_seprator_with_salt1}${md5_secret_2}${md5_seprator_with_salt2}${md5_secret_3}${md5_seprator_with_salt3}${md5_secret_4}${final_pepper_hash}`;
+const hashed_secret_1 = hash(secret_1);
+const hashed_secret_2 = hash(secret_2);
+const hashed_secret_3 = hash(secret_3);
+const hashed_secret_4 = hash(secret_4);
+const hashed_separator_with_salt1 = hash(`${separator}${separator_salt1}`);
+const hashed_separator_with_salt2 = hash(`${separator}${separator_salt2}`);
+const hashed_separator_with_salt3 = hash(`${separator}${separator_salt3}`);
+const final_pepper_hash = hash(final_pepper);
 
-//shuffling original key
+const original_key = `${hashed_secret_1}${hashed_separator_with_salt1}${hashed_secret_2}${hashed_separator_with_salt2}${hashed_secret_3}${hashed_separator_with_salt3}${hashed_secret_4}${final_pepper_hash}`;
+
+// Shuffling original key
 function shuffleString(key_to_shuffle) {
   // Convert string to array
-  let arr = key_to_shuffle.split("");
+  let arr = key_to_shuffle.split('');
 
   // Fisher-Yates shuffle algorithm
   for (let i = arr.length - 1; i > 0; i--) {
@@ -47,11 +45,10 @@ function shuffleString(key_to_shuffle) {
   }
 
   // Convert array back to string
-  return arr.join("");
+  return arr.join('');
 }
 
 // Example usage
-const originalString = original_key.slice(0, 255);
+const originalString = original_key;
 const shuffledString = shuffleString(originalString);
 console.log(shuffledString);
-
